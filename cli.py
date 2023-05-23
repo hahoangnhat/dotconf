408e864c07da4b10d98f68965214bad40759536f
Hà Hoàng Nhật <100401103+hahoangnhat@users.noreply.github.com>
2023-05-23 23:27:37 +0700

    Add cli

---
+++ cli.py
@@ -0,0 +1,49 @@
+import os
+import logging
+from argparse import ArgumentParser, RawTextHelpFormatter
+from utils import file
+from plugins import link, clean
+
+
+def add_options(parser):
+    parser.add_argument(
+        "-d", "--directory", type=ascii, help="Dotfile's base directory"
+    )
+    parser.add_argument("-c", "--config", type=ascii, help="Configuration filepath")
+    parser.add_argument("-a", "--action", type=ascii, help="Action")
+
+
+def main():
+    parser = ArgumentParser(RawTextHelpFormatter)
+    add_options(parser)
+    args = parser.parse_args()
+
+    # Add base directory to env
+    os.environ["BASE_DIRECTORY"] = args.directory
+
+    # Load file config
+    config = file.load(args.config)
+
+    # Default
+    if config["default"] != None or config["default"]:
+        default = config["default"]
+        # Overwrite
+        if default["overwrite"] != None or default["overwrite"]:
+            os.environ["OVERWRITE"] = default["overwrite"]
+
+    # Clean
+    if config["clean"] != None or config["clean"]:
+        for path in config["clean"]:
+            clean.remove_dead_links(path)
+
+    # Links
+    if config["links"] != None or config["links"]:
+        links = config["links"]
+
+        match args.action:
+            case "install":
+                link.create_links(links)
+                logging.info("Setup done")
+            case "uninstall":
+                link.remove_links(links)
+                logging.info("Uninstall done")
