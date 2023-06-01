472a189b5d39ada7473d0fbafdcbd0b314496b89
Hà Hoàng Nhật <100401103+hahoangnhat@users.noreply.github.com>
2023-06-01 23:33:33 +0700

    Fix logging

--- utils/logging.py
+++ utils/logging.py
@@ -7,6 +7,9 @@ config_file = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "logging.yaml"
 )
 
-with open(config_file, "r") as file:
-    config = yaml.safe_load(file.read())
-    logging.config.dictConfig(config)
+def getLogger(logger):
+    with open(config_file, "r") as file:
+        config = yaml.safe_load(file.read())
+        logging.config.dictConfig(config)
+
+    return logging.getLogger(logger)
