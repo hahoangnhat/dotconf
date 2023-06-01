0f9dfd86a2af0443dd05443fa2ce759b73512a69
Hà Hoàng Nhật <100401103+hahoangnhat@users.noreply.github.com>
2023-06-01 23:20:30 +0700

    Fix logging

--- utils/logging.py
+++ utils/logging.py
@@ -9,4 +9,5 @@ with open(config_file, "r") as file:
     config = yaml.safe_load(file.read())
     logging.config.dictConfig(config)
 
-logger = logging.getLogger("dotconf")
+global LOGGER
+LOGGER = logging.getLogger("dotconf")
